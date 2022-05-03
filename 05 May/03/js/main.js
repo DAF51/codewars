d5868d61d4f5968406d8915ee1d2b8c9b8244679
Daniel Figuereo <danielafig51@gmail.com>
2022-05-03 09:55:18 -0400

    May 3rd code war

---
+++ 05 May/03/js/main.js
@@ -0,0 +1,29 @@
+// Welcome.
+
+// In this kata you are required to, given a string, replace every letter with its position in the alphabet.
+
+// If anything in the text isn't a letter, ignore it and don't return it.
+
+// "a" = 1, "b" = 2, etc.
+// Example
+
+// alphabetPosition("The sunset sets at twelve o' clock.")
+
+// Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" ( as a string )
+
+function alphabetPosition(text) {
+  const alphabetArray = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+  let textArray =text.match(/[a-z]/gi)
+  if(textArray){
+    return textArray.map(currentVal => {
+      let letterNorm = currentVal.toLowerCase()
+      let index= alphabetArray.findIndex(letter => letter === letterNorm)
+      return ++index
+    }).join(" ")
+  } else {
+    return ""
+  }
+}
+
+
+
