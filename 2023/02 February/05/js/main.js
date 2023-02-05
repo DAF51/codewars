3a25917840de19555ddb032d4580aedb33e619e9
Daniel Figuereo <danielafig51@gmail.com>
2023-02-05 12:06:01 -0500

    Feb 05 code war

---
+++ 2023/02 February/05/js/main.js
@@ -0,0 +1,13 @@
+// Write a function reverse which reverses a list (or in clojure's case, any list-like data structure)
+
+// (the dedicated builtin(s) functionalities are deactivated)
+
+reverse = function(array) {
+  let newArr = []
+  array.forEach(val => {
+    newArr.unshift(val)
+  })
+  return newArr
+}
+
+
