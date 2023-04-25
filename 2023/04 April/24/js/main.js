da12f8755e296ab6e7c25c5233fb3e654cdb943d
Daniel Figuereo <danielafig51@gmail.com>
2023-04-25 10:50:23 -0400

    April 25 code war

---
+++ 2023/04 April/24/js/main.js
@@ -0,0 +1,23 @@
+// Complete the function that returns an array of length n, starting with 
+// the given number x and the squares of the previous number. If n is 
+// negative or zero, return an empty array/list.
+// Examples
+
+// 2, 5  -->  [2, 4, 16, 256, 65536]
+// 3, 3  -->  [3, 9, 81]
+
+
+function squares(x, n) {
+  if(n<=0){
+    return []
+  }
+  let arr = [x]
+  let length = arr.length
+  while(length < n){
+    let next = arr[length-1]**2
+    arr.push(next)
+    length = arr.length
+  }
+  return arr
+}
+
